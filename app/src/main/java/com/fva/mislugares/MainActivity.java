2703a92e19e1b225f075b0e740111198b03259ab
Franz Villalpando <villalpando.franz@usfx.bo>
2017-10-20 18:05:34 -0400

    Un RecyclerView en Mis Lugares

--- app/src/main/java/com/fva/mislugares/MainActivity.java
+++ app/src/main/java/com/fva/mislugares/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -14,11 +16,20 @@ import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    public AdaptadorLugares adaptador;
+    private RecyclerView.LayoutManager layoutManager;
     public static Lugares lugares = new LugaresVector();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adaptador = new AdaptadorLugares(this, lugares);
+        recyclerView.setAdapter(adaptador);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
        // setContentView(R.layout.edicion_lugar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
